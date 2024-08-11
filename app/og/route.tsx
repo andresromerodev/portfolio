a43806802a97cd88aa2d0c45c022ebfd746b378d
Andrés Romero <andresromero.dev@gmail.com>
2024-08-11 05:38:08 -0600

    chore(og): update route.tsx

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -8,7 +8,7 @@ export function GET(request: Request) {
     (
       <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h1 tw="flex flex-col ml-8 text-4xl font-bold tracking-tight text-left">
+          <h1 tw="flex flex-col ml-12 text-6xl font-bold tracking-tight text-left">
             {title}
           </h1>
         </div>
