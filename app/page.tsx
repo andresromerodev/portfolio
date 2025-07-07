df08faf98d3ace8baca6d3fdd4ee2155a187c877
andresromerodev <andresromero.dev@gmail.com>
2025-07-06 21:27:11 -0600

    chore(md): update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         Hey, I'm Andrés! I'm a Software Engineer from Costa Rica.
         <br />
         <br />
-        I currently work at <a href='https://www.lunchboxentertainment.com'target='_blank'><u>Lunchbox</u></a> where I'm part of the Game Services Team building the future of competitive gaming with <a href='https://store.steampowered.com/app/2790090/Sirocco'target='_blank'><u>Sirocco</u></a> a revolutionary new MOBA.
+        I currently work at <u>Skymap Games</u> where I'm part of the Server Engineering team co-developing an exciting unannounced AAA game title.
         <br />
         <br />
         From coding to leading, I've got the full stack covered - take a look at my <a href='https://andresromerodev.notion.site/andresromerodev/Andr-s-Romero-1c7e5808a418431b8df0dd4fa63c25ba'target='_blank'><u>Resume</u></a> and let's discuss that idea you have in mind!
