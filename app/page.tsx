bce0f96fcc9f28f5ca74014702946d524658b3e5
andresromerodev <andresromero.dev@gmail.com>
2025-08-03 15:06:29 -0600

    feat(md): update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         {"</>"} Andresromero.dev
       </h1>
       <p className="mb-4">
-        Hey, I'm Andrés, a Software Engineer from Costa Rica.
+        Hey, I'm Andrés! a Software Engineer from Costa Rica.
         <br />
         <br />
         I work at <u>Skymap Games</u> where I’m part of the Server Engineering team building the backend for an unannounced AAA game (yes, it’s as cool as it sounds).
@@ -17,7 +17,7 @@ export default function Page() {
         <br />
         <br />
         This blog is where I share what I’ve learned, what I’m working on, and sometimes just what I find interesting in tech and games.
-         Take a look around, and if something sparks an idea, let’s connect.
+        Take a look around, and if something sparks an idea, let’s connect.
        </p>
       <hr />
 
