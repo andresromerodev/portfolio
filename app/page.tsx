76203b0e2f24fa91008883d9048a60fbe1226b18
andresromerodev <andresromero.dev@gmail.com>
2025-08-06 05:00:37 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         Hey, I'm Andrés! a Software Engineer from Costa Rica.
         <br />
         <br />
-        I work at <b>Skymap Games</b> where I’m part of the Server Engineering team building the backend for an unannounced AAA game.
+        I work at <b>Skymap Games</b> where I’m part of the Engineering team building the backend and infrastructure for a new unannounced AAA game.
         <br />
         <br />
         I’m into clean code, AI, distributed systems, and building things that actually work at scale. From backend logic to full-stack problem-solving, I like turning complex problems into real world solutions.
